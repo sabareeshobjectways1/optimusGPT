540f7a9f1c6e84453920852a91c630294404db9e
sabareeshobjectways1 <sabareeshobjectways@gmail.com>
2025-01-04 17:17:07 +0000

    fourth

--- src/components/Drawer.tsx
+++ src/components/Drawer.tsx
@@ -44,6 +44,15 @@ export function Drawer({ isOpen, onClose }: DrawerProps) {
               <ModelSelector />
             </div>
 
+            <div className="space-y-4">
+             
+              
+              <ThemeToggle />
+
+             
+
+             
+            </div>
           </div>
 
           <div className="mt-auto pt-6 border-t border-gray-200 dark:border-gray-700">
